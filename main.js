de2d4eab413b06ac03bbf043632127272218a148
kartikeybca <135728169+kartikeybca@users.noreply.github.com>
2023-06-06 21:35:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// change navbar styles in scroll
+const navbar = document.querySelector("nav") 
+
+window.addEventListener("scroll", ()=>{
+    navbar.classList.toggle("window-scroll", window.scrollY > 1);
+})
+ 
+// show/hide faq answer
+const faq = document.querySelectorAll(".faq");
+faq.forEach(faq => {
+    faq.addEventListener("click",()=>{
+        faq.classList.toggle("open");
+
+        // icon change
+        const icon = faq.querySelector(".faq_icon i");
+        if(icon.className === "uil uil-plus"){
+            icon.className = "uil uil-minus";
+        } else{
+            icon.className = "uil uil-plus";
+        }
+    })
+});
+
+// show/hide nav menu 
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener("click", ()=>{
+    menu.style.display="flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display="none";
+})
+closeBtn.addEventListener("click", ()=>{
+    menu.style.display="none";
+    closeBtn.style.display="none";
+    menuBtn.style.display="inline-block";
+});
+
